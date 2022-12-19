b9ab0a29f363fa1d3d2a1ae718a3c05eea8c6a21
Freeze096 <118058868+Freeze096@users.noreply.github.com>
2022-12-19 12:17:10 +0800

    Add files via upload

---
+++ Simple_Chinese_Timer1.1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<unistd.h>
+#include<windows.h>
+using namespace std;
+int main(){
+	int a;
+	cout<<"请输入倒计时时间（单位：秒）：";
+	cin>>a;
+	if(MessageBox(0,"是否开始？（开始按确定）","对话框",MB_YESNO)==6){
+		for(int i=a;i>=1;i--){
+		
+		cout<<"还剩"<<i<<"秒"<<endl; 
+		sleep(1);
+	} 
+	MessageBox(0,"计时结束","提示",MB_OK);
+	} else{
+		
+	}
+	
+} 
